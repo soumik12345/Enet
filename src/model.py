98ebc131b82aa391f2fa264d91da35f03b6cb6b4
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2019-12-10 02:58:41 +0530

    Added Enet Forward Pass

--- src/model.py
+++ src/model.py
@@ -120,10 +120,53 @@ class Enet(Module):
 		self.upsample_5 = UpsampleBottleneckBlock(
 			64, 16, dropout_prob=0.1, relu=decoder_relu
 		)
-		self.bottleneck_5_1 = RegularBottleneckBlock(
+		self.bottleneck_5 = RegularBottleneckBlock(
 			16, padding=1, dropout_prob=0.1, relu=decoder_relu
 		)
 		self.transposed_conv = ConvTranspose2d(
 			16, num_classes, kernel_size=3,
             stride=2, padding=1, bias=False
 		)
+	
+
+	def forward(self, x):
+		'''Forward pass for Enet'''
+		# Initial Block
+		input_size = x.size()
+		x = self.initial_block(x)
+		# Stage 1
+		input_size_1 = x.size()
+		x, max_indices_1 = self.down_bottleneck_1(x)
+		x = self.bottleneck_1_1(x)
+		x = self.bottleneck_1_2(x)
+		x = self.bottleneck_1_3(x)
+		x = self.bottleneck_1_4(x)
+		# Stage 2
+		input_size_2 = x.size()
+		x, max_indices_2 = self.down_bottleneck_2(x)
+		x = self.bottleneck_2_1(x)
+		x = self.bottleneck_2_2(x)
+		x = self.bottleneck_2_3(x)
+		x = self.bottleneck_2_4(x)
+		x = self.bottleneck_2_5(x)
+		x = self.bottleneck_2_6(x)
+		x = self.bottleneck_2_7(x)
+		x = self.bottleneck_2_8(x)
+		# Stage 3
+		x = self.regular_bottleneck_3(x)
+		x = self.bottleneck_3_1(x)
+		x = self.bottleneck_3_2(x)
+		x = self.bottleneck_3_3(x)
+		x = self.bottleneck_3_4(x)
+		x = self.bottleneck_3_5(x)
+		x = self.bottleneck_3_6(x)
+		x = self.bottleneck_3_7(x)
+		# Stage 4
+		x = self.upsample_4(x)
+		x = self.bottleneck_4_1(x)
+		x = self.bottleneck_4_2(x)
+		# Stage 5
+		x = self.upsample_5(x)
+		x = self.bottleneck_5(x)
+		x = self.transposed_conv(x)
+		return x
