239facf250ffe232ee661a2368c9dd5d69db6c2a
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2019-12-09 23:24:55 +0530

    Added Downsample Bottleneck Block

--- src/blocks.py
+++ src/blocks.py
@@ -129,3 +129,96 @@ class RegularBottleneck(nn.Module):
 		output = main_brach + secondary_brach
 		output = self.activation(output)
 		return output
+
+
+
+class DownsampleBottleneckBlock(Module):
+
+	def __init__(
+		self, in_channels, out_channels, internal_ratio=4,
+		return_indices=False, dropout_prob=0, bias=False, relu=True):
+		'''Enet DownSampling Bottleneck Block
+		Reference: https://arxiv.org/abs/1606.02147
+		Params:
+			in_channels		-> Number of input channels
+			out_channels	-> Number of output channels
+			internal_ratio	-> Scale factor for channels
+			return_indices	-> Returns max indices if true
+			dropout_prob	-> Probability for dropout
+			bias			-> Use a bias or not
+			relu			-> Use ReLU activation if true
+		'''
+		super().__init__()
+		internal_channels = channels // internal_ratio
+		self.return_indices = return_indices
+
+		### Main Branch ###
+
+		# Block 1 Conv 1x1
+		self.main_conv_block_1 = Sequential(
+			Conv2d(
+				in_channels, internal_channels,
+				kernel_size=2, stride=2, bias=bias
+			),
+			BatchNorm2d(internal_channels),
+			ReLU() if relu else PReLU()
+		)
+
+		# Block 2 Conv 3x3
+		self.main_conv_block_2 = Sequential(
+			Conv2d(
+				internal_channels, internal_channels,
+				kernel_size=3, stride=1, padding=1, bias=bias
+			),
+			BatchNorm2d(internal_channels),
+			ReLU() if relu else PReLU()
+		)
+
+		# Block 2 Conv 1x1
+		self.main_conv_block_3 = Sequential(
+			Conv2d(
+				internal_channels, out_channels,
+				kernel_size=1, stride=1, bias=bias
+			),
+			BatchNorm2d(out_channels),
+			ReLU() if relu else PReLU()
+		)
+
+		### Secondary Branch ###
+		self.secondary_maxpool = MaxPool2d(
+			2, stride=2,
+			return_indices=return_indices
+		)
+
+		# Dropout Regularization
+		self.dropout = Dropout2d(p=dropout_prob)
+
+		# Activation
+		self.activation = ReLU() if relu else PReLU()
+	
+
+	def forward(self, x):
+		'''Forward Pass for DownsampleBottleneckBlock'''
+		# Main Branch
+		main_brach = self.main_conv_block_1(x)
+		main_brach = self.main_conv_block_2(main_brach)
+		main_brach = self.main_conv_block_3(main_brach)
+		# Secondary Branch
+		if self.return_indices:
+			secondary_brach, max_indices = self.secondary_maxpool(x)
+		else:
+			secondary_branch = self.secondary_maxpool(x)
+		# Padding
+		n, ch_main, h, w = main_brach.size()
+		ch_sec = secondary_branch.size()[1]
+		padding = torch.zeros(n, ch_main - ch_sec, h, w)
+		if secondary_branch.is_cuda:
+			padding = padding.cuda()
+		# Concatenate
+		secondary_branch = torch.cat((secondary_branch, padding), 1)
+		output = secondary_branch + primary_branch
+		output = self.activation(output)
+		if self.return_indices:
+			return output, max_indices
+		else:
+			return output
