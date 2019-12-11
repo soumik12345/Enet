835e773c88fe1e93b33239adc6540cec334d86d5
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2019-12-11 12:27:00 +0530

    Added Decoder for Segmap

--- src/camvid_pipeline.py
+++ src/camvid_pipeline.py
@@ -15,6 +15,54 @@ def get_class_weights(loader, num_classes, c=1.02):
     return class_weights
 
 
+def read_image(image_file):
+    image = Image.open(image_file)
+    image = np.array(image)
+    image = cv2.resize(image, (512, 512), cv2.INTER_LINEAR)
+    image = torch.tensor(image).unsqueeze(0).float()
+    image = image.transpose(2, 3).transpose(1, 2).to(device)
+    return image
+
+
+def decode_segmap(image):
+    Sky = [128, 128, 128]
+    Building = [128, 0, 0]
+    Pole = [192, 192, 128]
+    Road_marking = [255, 69, 0]
+    Road = [128, 64, 128]
+    Pavement = [60, 40, 222]
+    Tree = [128, 128, 0]
+    SignSymbol = [192, 128, 128]
+    Fence = [64, 64, 128]
+    Car = [64, 0, 128]
+    Pedestrian = [64, 64, 0]
+    Bicyclist = [0, 128, 192]
+
+    label_colours = np.array([Sky, Building, Pole, Road_marking, Road, 
+                              Pavement, Tree, SignSymbol, Fence, Car, 
+                              Pedestrian, Bicyclist]).astype(np.uint8)
+    r = np.zeros_like(image).astype(np.uint8)
+    g = np.zeros_like(image).astype(np.uint8)
+    b = np.zeros_like(image).astype(np.uint8)
+    for l in range(0, 12):
+        r[image == l] = label_colours[l, 0]
+        g[image == l] = label_colours[l, 1]
+        b[image == l] = label_colours[l, 2]
+
+    rgb = np.zeros((image.shape[0], image.shape[1], 3)).astype(np.uint8)
+    rgb[:, :, 0] = b
+    rgb[:, :, 1] = g
+    rgb[:, :, 2] = r
+    return rgb
+
+
+def predict_rgb(model, tensor):
+    with torch.no_grad():
+        out = model(tensor.float()).squeeze(0)
+    out = out.data.max(0)[1].cpu().numpy()
+    return decode_segmap(out)
+
+
 class CamVidDataset(Dataset):
 
 	def __init__(self, images, labels, height, width):
